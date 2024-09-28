559095f157bc6614a0eba64917d339b282512ab5
Jiwon <ysjjw2003@naver.com>
2024-09-29 01:07:53 +0900

    add gitignore

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,8 +7,7 @@ const Hero = () => {
         <h3>안녕하세요!</h3>
         <h3>제 이름은 전지원입니다.</h3>
         <h3>소통하는 개발자가 되고 싶어요.</h3>
-
-        
+        {/* <img src="emoji1.png" alt="emoji1" /> */}
       </div>
     </div>
   );
