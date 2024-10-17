13b856d6f59a9b08e0db11f7b1d0e9ae5f41ab82
Jiwon <ysjjw2003@naver.com>
2024-10-17 17:48:12 +0900

    FIX goal

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
       <div className="Hero" id="home">
         <h2>안녕하세요!</h2>
         <h2>제 이름은 전지원입니다.</h2>
-        <h2>소통하는 개발자가 되고 싶어요.</h2>
+        <h2>성장하는 개발자가 되고 싶어요.</h2>
         <img src={emoji1} alt="emoji1" />
       </div>
     </div>
--- src/pages/Notfound.jsx
+++ src/pages/Notfound.jsx
@@ -1,5 +1,5 @@
 const Notfound = () => {
-  <div>잘못된 페이지입니다.</div>;
+  return <div className="notfound">잘못된 페이지입니다.</div>;
 };
 
 export default Notfound;
