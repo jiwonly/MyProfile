531f2fb2f41786895ac980dfdac248ca85cbe750
Jiwon <ysjjw2003@naver.com>
2024-10-20 12:03:33 +0900

    FIX goal

--- src/components/Goal.jsx
+++ src/components/Goal.jsx
@@ -7,12 +7,9 @@ const Goal = () => {
       <div className="goal_title">
         <h2>GOAL</h2>
         <div className="goal_list">
-          <GoalData title="React 복습하기" content={"강의 다시 듣기"} />
+          <GoalData title="React 복습하기" content="강의 다시 듣기" />
           <GoalData title="Node.js 복습하기" content="자료 찾아보기" />
-          <GoalData
-            title="AWS 정복하기"
-            content="강의 자료 찾아보기"       
-          />
+          <GoalData title="AWS 정복하기" content="강의 자료 찾아보기" />
         </div>
       </div>
     </div>
